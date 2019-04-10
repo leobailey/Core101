2077a3e7db118bfc6935be36da2462736ab78065
Leo Bailey <leobailey@hotmail.com>
2019-04-10 17:43:38 +0100

    express api basic implementation

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-var app = new Vue({
-    el: '#app',
+
+/* var myVue = new Vue({
+    el: '#vueapp',
     data: {
       message: 'Hello from Vue! we meet again!!'
     }
+  }) */
+
+  var express = require('express');
+  var app  = express();
+  var port = process.env.PORT || 3000
+  app.get('/', (req, res) => {
+    res.send('Welcome to my API');
   })
+
+  app.listen(port, ()=>{
+    console.log('Running on port ' + port);
+  });
