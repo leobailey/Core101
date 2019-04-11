28b04bc67893b492fcc4468e2cc0395ca162256b
Leo Bailey <leobailey@hotmail.com>
2019-04-11 13:43:57 +0100

    implement demo router

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
 const app = express();
+const demoRouter = express.Router();
 const port = process.env.PORT || 3000;
+
+demoRouter.route('/demo')
+.get((req, res) => {
+  const response = { hello: 'This is my demo API'};
+  res.json(response);
+});
+
+app.use('/api', demoRouter);
+
 app.get('/', (req, res) => {
-  res.send('Welcome to my autom updating API');
+  res.send('Welcome to my auto updating API');
 });
 
 app.listen(port, () => {
